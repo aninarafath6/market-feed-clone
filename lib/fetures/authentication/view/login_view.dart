150c6101ac33cab0c27a07c336ed96781ebe5ac0
aninarafath6 <arafathanin6@gmail.com>
2021-10-19 08:52:07 +0530

    logi section

--- lib/fetures/authentication/view/login_view.dart
+++ lib/fetures/authentication/view/login_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:marketfeed_clone/common/constants/app_colors.dart';
 import 'package:marketfeed_clone/common/constants/app_images.dart';
 import 'package:marketfeed_clone/fetures/authentication/widgets/bottom_section.dart';
@@ -15,19 +16,48 @@ class LoginView extends StatelessWidget {
         context: context,
         builder: (context) {
           return AlertDialog(
-            title: const Text('Exit'),
+            backgroundColor: AppColors.grey,
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Exit',
+                  style: GoogleFonts.roboto(
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 24),
+                Text(
+                  'Are you sure you want to exit?',
+                  style: GoogleFonts.roboto(
+                    color: Colors.white,
+                    fontSize: 17,
+                  ),
+                ),
+              ],
+            ),
             actions: [
               TextButton(
                 onPressed: () {
                   Navigator.of(context).pop(false);
                 },
-                child: const Text('NO'),
+                child: const Text(
+                  'NO',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
               ),
               TextButton(
                 onPressed: () {
                   Navigator.of(context).pop(true);
                 },
-                child: const Text('YES'),
+                child: const Text(
+                  'YES',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ],
           );
